b78e2d2aa6fad1d0d929ecfbdc1e1c4b41b2e127
Arseny Bochkarev <bochkarevarseny@gmail.com>
2024-12-22 19:07:10 +0300

    Add parallel file reading in reducer

--- reducer.cpp
+++ reducer.cpp
@@ -5,33 +5,44 @@
 #include <vector>
 #include <algorithm>
 #include <execution>
+#include <mutex>
+
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
+
+std::mutex mtx;
 
 int main() {
+    const int threadsNum = 4;
+    boost::asio::thread_pool pool(threadsNum);
     std::string line;
     std::map<std::string, std::pair<double, int>> category_data;
 
-    // TODO: parallel file reading
     while (std::getline(std::cin, line)) {
-        std::istringstream sstream(line);
-        std::string category, revenue_str, quantity_str;
+        boost::asio::post(pool, [line, &category_data]() -> void {
+            std::istringstream sstream(line);
+            std::string category, revenue_str, quantity_str;
 
-        std::getline(sstream, category, ',');
-        std::getline(sstream, revenue_str, ',');
-        std::getline(sstream, quantity_str, ',');
+            std::getline(sstream, category, ',');
+            std::getline(sstream, revenue_str, ',');
+            std::getline(sstream, quantity_str, ',');
 
-        double revenue = stod(revenue_str);
-        int quantity = stoi(quantity_str);
-
-        category_data[category].first += revenue;
-        category_data[category].second += quantity;
+            double revenue = stod(revenue_str);
+            int quantity = stoi(quantity_str);
+            {
+                std::lock_guard<std::mutex> lck(mtx);
+                category_data[category].first += revenue;
+                category_data[category].second += quantity;
+            }
+        });
     }
+    pool.join();
 
     // Sorting data
     std::vector<std::pair<std::string, std::pair<double, int>>> sorted_data;
     for (const auto& entry : category_data) {
         sorted_data.emplace_back(entry.first, entry.second);
     }
-
     // Parallel sort using execution policy
     // Note: to compile for it, use `-std=c++17` and `-ltbb` flags (make sure to have libtbb-dev installed!)
     std::sort(std::execution::par_unseq, sorted_data.begin(), sorted_data.end(), [](const auto& a, const auto& b) { return a.second.first > b.second.first; });
