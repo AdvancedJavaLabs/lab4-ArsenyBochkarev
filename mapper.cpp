643a4bf7834a0f159214407b502684b6b83ae20e
Arseny Bochkarev <bochkarevarseny@gmail.com>
2024-12-22 18:57:15 +0300

    Add parallel file reading in mapper

--- mapper.cpp
+++ mapper.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <mutex>
+
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
+
+std::mutex mtx;
 
 int main() {
+    const int threadsNum = 4;
+    boost::asio::thread_pool pool(threadsNum);
     std::string line;
-    // TODO: parallel file reading
     while (std::getline(std::cin, line)) {
-        std::istringstream sstream(line);
-        std::string transaction_id, product_id, category, price_str, quantity_str;
-        std::getline(sstream, transaction_id, ',');
-        std::getline(sstream, product_id, ',');
-        std::getline(sstream, category, ',');
-        std::getline(sstream, price_str, ',');
-        std::getline(sstream, quantity_str, ',');
+        boost::asio::post(pool, [line]() -> void {
+            std::istringstream sstream(line);
+            std::string transaction_id, product_id, category, price_str, quantity_str;
+            std::getline(sstream, transaction_id, ',');
+            std::getline(sstream, product_id, ',');
+            std::getline(sstream, category, ',');
+            std::getline(sstream, price_str, ',');
+            std::getline(sstream, quantity_str, ',');
 
-        double price = std::stod(price_str);
-        int quantity = std::stoi(quantity_str);
-
-        std::cout << category << "," << price * quantity << "," << quantity << std::endl;
+            double price = std::stod(price_str);
+            int quantity = std::stoi(quantity_str);
+            {
+                std::lock_guard<std::mutex> lck(mtx);
+                std::cout << category << "," << price * quantity << "," << quantity << "\n";
+            }
+        });
     }
+
+    pool.join();
     return 0;
 }
